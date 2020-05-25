d58d6f3a67028c8e8ca9bfc2673acd61416e7637
Logicless-Coder <59042344+Logicless-Coder@users.noreply.github.com>
2020-05-25 11:44:07 +0530

    Add files via upload

---
+++ precache-manifest.c8662c9e267acafb1ec729adf8942336.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b5e05d2c6adf2a4e6878e1756ad4db",
+    "url": "/Simple-Calculator-React/index.html"
+  },
+  {
+    "revision": "174c7f16eadafa9bda8f",
+    "url": "/Simple-Calculator-React/static/css/main.92b71311.chunk.css"
+  },
+  {
+    "revision": "2e49c435d17cc5717dc3",
+    "url": "/Simple-Calculator-React/static/js/2.08589ab4.chunk.js"
+  },
+  {
+    "revision": "26cea779844b95048ab44bfc1acedf77",
+    "url": "/Simple-Calculator-React/static/js/2.08589ab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "174c7f16eadafa9bda8f",
+    "url": "/Simple-Calculator-React/static/js/main.ab738462.chunk.js"
+  },
+  {
+    "revision": "5e474d316a313563667a",
+    "url": "/Simple-Calculator-React/static/js/runtime-main.d4c8aba4.js"
+  }
+]);
